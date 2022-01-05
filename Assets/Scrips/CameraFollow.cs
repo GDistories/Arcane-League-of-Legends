31208176ef777c26e3062085234f5d92f66e8c90
GDistories <18663977305@163.com>
2022-01-05 21:37:32 +0800

    Add Camera Limit

--- Assets/Scrips/CameraFollow.cs
+++ Assets/Scrips/CameraFollow.cs
@@ -8,6 +8,10 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform target;
 
     [SerializeField] private float smoothing;
+
+    [SerializeField] private Vector2 minPosition;
+
+    [SerializeField] private Vector2 maxPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +31,17 @@ public class CameraFollow : MonoBehaviour
             if (transform.position != target.position)
             {
                 Vector3 targetPos = target.position;
+                targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+                targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
                 transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
                 
             }
         }
+    }
+
+    public void SetCamPosLimit(Vector2 minPos, Vector2 maxPos)
+    {
+        minPosition = minPos;
+        maxPosition = maxPos;
     }
 }
