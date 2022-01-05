5ac435df149c2be66782dc914446245812afda28
GDistories <18663977305@163.com>
2022-01-05 22:50:51 +0800

    Update Player Health

--- Assets/Scrips/Player/PlayerHealth.cs
+++ Assets/Scrips/Player/PlayerHealth.cs
@@ -6,16 +6,17 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int health = 10;
-
+    [SerializeField] private int Blinks;
+    [SerializeField] private float time;
     [SerializeField] private float timeBetweenDamage = 1f;
 
     private float nextDamageTime = 0;
-
+    private Renderer myRenderer;
     private bool canTakeDamage = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -29,6 +30,7 @@ public class PlayerHealth : MonoBehaviour
         if (canTakeDamage)
         {
             health -= damage;
+            BlinkPlayer(Blinks, time);
             canTakeDamage = false;
             nextDamageTime = timeBetweenDamage + Time.time;
         }
@@ -44,5 +46,21 @@ public class PlayerHealth : MonoBehaviour
         {
             canTakeDamage = true;
         }
+    }
+
+    private void BlinkPlayer(int numBlinks, float seconds)
+    {
+        StartCoroutine(DoBlinks(numBlinks, seconds));
+    }
+
+    IEnumerator DoBlinks(int numBlinks, float seconds)
+    {
+        for (int i = 0; i < numBlinks * 2; i++)
+        {
+            myRenderer.enabled = !myRenderer.enabled;
+            yield return new WaitForSeconds(seconds);
+        }
+
+        myRenderer.enabled = true;
     }
 }
