339251d2d98e5f60ea5c59218f9006722a280173
GDistories <18663977305@163.com>
2022-01-07 15:18:35 +0800

    Add Tutorial

--- Assets/Scrips/Enemy/EnemyBeetle.cs
+++ Assets/Scrips/Enemy/EnemyBeetle.cs
@@ -74,7 +74,7 @@ public class EnemyBeetle : Enemy
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.BoxCollider2D")
         {
             playerController.myAnimator.SetBool("IsJump", true);
             playerController.myAnimator.SetBool("IsDoubleJump", true);
